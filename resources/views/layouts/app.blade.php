27e52f62c609d6741fe4ad28753fdeacee86a318
Anugrah Tejo Maliki <tejomaliki1@email.com>
2026-03-13 15:45:28 +0700

    oke

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -364,7 +364,7 @@
                     <span class="drawer-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-[#0B5E41]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"/></svg>
                     </span>
-                    Sosial Media
+                    MediaSosial
                 </a>
                 <a href="{{ route('landing') }}#lokasi" class="drawer-link">
                     <span class="drawer-icon">
