35b0135eb9435cdbe3e412a328455bc5a82e138b
Anugrah Tejo Maliki <tejomaliki1@email.com>
2026-03-17 21:17:10 +0700

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,8 +29,28 @@
                             <a href="#" class="text-[#676767] text-xs hover:text-[#393939] transition-colors">forgot
                                 password?</a>
                         </div>
-                        <input type="password" name="password" placeholder="••••••••" class="input-field" required
-                            autocomplete="current-password">
+                        <div class="relative">
+                            <input type="password" id="password" name="password" placeholder="••••••••" class="input-field pr-11" required
+                                autocomplete="current-password">
+                            <button type="button" id="toggle-password"
+                                class="absolute inset-y-0 right-3 flex items-center text-[#676767] hover:text-[#393939] transition-colors"
+                                onclick="togglePassword()">
+                                {{-- Eye open --}}
+                                <svg id="icon-eye" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                                {{-- Eye slash --}}
+                                <svg id="icon-eye-slash" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hidden"
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.978 9.978 0 012.082-3.416M6.53 6.53A9.956 9.956 0 0112 5c4.477 0 8.268 2.943 9.542 7a9.972 9.972 0 01-4.073 5.373M15 12a3 3 0 00-3-3m0 0a3 3 0 00-2.121.879M3 3l18 18" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
 
                     {{-- Submit --}}
@@ -41,5 +61,25 @@
             </div>
         </div>
     </div>
+
+@section('scripts')
+<script>
+    function togglePassword() {
+        const input = document.getElementById('password');
+        const eyeOpen = document.getElementById('icon-eye');
+        const eyeSlash = document.getElementById('icon-eye-slash');
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            eyeOpen.classList.add('hidden');
+            eyeSlash.classList.remove('hidden');
+        } else {
+            input.type = 'password';
+            eyeOpen.classList.remove('hidden');
+            eyeSlash.classList.add('hidden');
+        }
+    }
+</script>
+@endsection
 
 @endsection
